23cdca86863dce448ddc7c6bd81ad7b8ea05c0fb
Chen Jian <lsdlab@icloud.com>
2017-05-20 02:22:15 +0800

    fix some bug

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # 单干小雷达
 
-![Language](https://img.shields.io/badge/language-Node.js-brightgreen.svg?style=flat-square)
 ![License](https://img.shields.io/badge/license-MIT-blue.svg?style=flat-square)
 
 微信小程序：与自由工作者、程序员、设计师分享适合办公的地点。
--- pages/user_detail/user_detail.js
+++ pages/user_detail/user_detail.js
@@ -10,27 +10,36 @@ Page({
   onLoad: function (options) {
     console.log('onLoad')
     var that = this
-    var user_id = options.user_id
-    var user_detail_request_url = "http://dangann.com/api/v1/users/" + user_id + "/?format=json"
-    wx.request({
-      url: user_detail_request_url,
-      header: {
-        'content-type': 'application/json',
-        'Authorization': 'JWT ' + wx.getStorageSync('api_token')
-      },
-      success: function(res) {
-        var user_detail = res.data
-        var shareData = {}
-          shareData.title = user_detail.weixin_nickName
-          shareData.path = '/pages/user_detail/user_detail?user_id=' + user_id.toString()
-          that.setData({
-            shareData: shareData,
-          })
-        that.setData({
-          user_detail: user_detail
-        })
-      }
+    var user_id = options.user_id.split('@')[0]
+    var user_name = options.user_id.split('@')[1]
+    var user_detail = {}
+    if (user_id) {
+      var user_detail_request_url = "http://dangann.com/api/v1/users/" + user_id + "/?format=json"
+      wx.request({
+        url: user_detail_request_url,
+        header: {
+          'content-type': 'application/json',
+          'Authorization': 'JWT ' + wx.getStorageSync('api_token')
+        },
+        success: function(res) {
+          user_detail = res.data
+          var shareData = {}
+            shareData.title = user_detail.weixin_nickName
+            shareData.path = '/pages/user_detail/user_detail?user_id=' + user_id.toString()
+            that.setData({
+              shareData: shareData,
+            })
+
+        }
+      })
+    } else {
+      user_detail['weixin_nickName'] = user_name
+    }
+
+    that.setData({
+      user_detail: user_detail
     })
+
 
 
   },
