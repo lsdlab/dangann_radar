9e401c8d740808c5042004882ac582385350d4be
Chen Jian <lsdlab@icloud.com>
2017-06-09 13:25:29 +0800

    some update

--- README.md
+++ README.md
@@ -4,15 +4,13 @@
 
 ![License](https://img.shields.io/badge/license-MIT-blue.svg?style=flat-square)
 
-微信小程序：与自由工作者、程序员、设计师分享适合办公的地点。
+微信小程序：与自由工作者、程序员、设计师分享适合外出办公的地点。
 
 数据来源于: [ElaWorkshop/awesome-cn-cafe](https://github.com/ElaWorkshop/awesome-cn-cafe)
 
 已经上线：
 
 ![weixin-qrcode](https://breakwire.me/images/dangann/weixin-qrcode.png)
-
-
 
 ![Group@2x](https://breakwire.me/images/dangann/Group@2x.png)
 
@@ -21,3 +19,5 @@
 ![Group3@2x](https://breakwire.me/images/dangann/Group3@2x.png)
 
 ![Group4@2x](https://breakwire.me/images/dangann/Group4@2x.png)
+
+
--- pages/spot_detail/spot_detail.js
+++ pages/spot_detail/spot_detail.js
@@ -51,44 +51,56 @@ Page({
     var spot_id = event.currentTarget.id.split('@')[0]
     var spot_name = event.currentTarget.id.split('@')[1]
     var spot_city = event.currentTarget.id.split('@')[2]
-    var check_request_url = "https://dangann.com/api/v1/comments/?format=json"
-    wx.request({
-      method: 'POST',
-      data: {
-         'spot_id': spot_id,
-         'comment_message': '',
-         'comment_user_id': wx.getStorageSync('user_data').id,
-         'comment_user_name': wx.getStorageSync('user_data').username,
-         'comment_user_avatarurl': wx.getStorageSync('userInfo').avatarUrl,
-         'comment_mark': 'check',
-         'spot_city': spot_city
-      },
-      url: check_request_url,
-      header: {
-        'content-type':'application/x-www-form-urlencoded',
-        'Authorization': 'JWT ' + wx.getStorageSync('api_token')
-      },
-      success: function(res) {
-        var check_data = res.data
-        console.log('check_data')
-        console.log(check_data)
 
-        var current_date = new Date()
-        var current_date_string = current_date.getFullYear().toString() + current_date.getMonth().toString() + current_date.getDay().toString()
-        var today_spot_checked_mark = spot_id.toString() + current_date_string + 'checked'
-        wx.setStorageSync(today_spot_checked_mark, 'checked')
+    var current_date = new Date()
+    var current_date_string = current_date.getFullYear().toString() + current_date.getMonth().toString() + current_date.getDay().toString()
+    var today_spot_checked_mark = spot_id.toString() + current_date_string + 'checked'
+    if (wx.getStorageSync(today_spot_checked_mark) == '') {
+      var check_request_url = "https://dangann.com/api/v1/comments/?format=json"
+      wx.request({
+        method: 'POST',
+        data: {
+           'spot_id': spot_id,
+           'comment_message': '',
+           'comment_user_id': wx.getStorageSync('user_data').id,
+           'comment_user_name': wx.getStorageSync('user_data').username,
+           'comment_user_avatarurl': wx.getStorageSync('userInfo').avatarUrl,
+           'comment_mark': 'check',
+           'spot_city': spot_city
+        },
+        url: check_request_url,
+        header: {
+          'content-type':'application/x-www-form-urlencoded',
+          'Authorization': 'JWT ' + wx.getStorageSync('api_token')
+        },
+        success: function(res) {
+          var check_data = res.data
+          console.log('check_data')
+          console.log(check_data)
 
-        wx.showToast({
-          title: '已签到',
-          icon: 'success',
-          duration: 2000
-        });
+          var current_date = new Date()
+          var current_date_string = current_date.getFullYear().toString() + current_date.getMonth().toString() + current_date.getDay().toString()
+          var today_spot_checked_mark = spot_id.toString() + current_date_string + 'checked'
+          wx.setStorageSync(today_spot_checked_mark, 'checked')
 
-        that.setData({
-          checked_text: '已签到'
-        })
-      }
-    })
+          wx.showToast({
+            title: '已签到',
+            icon: 'success',
+            duration: 2000
+          });
+
+          that.setData({
+            checked_text: '已签到'
+          })
+        }
+      })
+    } else {
+      wx.showToast({
+        title: '今天已签到，请勿重复签到',
+        icon: 'success',
+        duration: 2000
+      });
+    }
   },
   controltap: function(e) {
     console.log(e.controlId)
@@ -115,7 +127,7 @@ Page({
 
     if (wx.getStorageSync(today_spot_checked_mark) == 'checked') {
       that.setData({
-        checked_or_not: 'checked'
+        checked_text: '已签到'
       })
     }
 
